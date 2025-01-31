212ff6310da2bf64b2afcfa843486fe81516c584
Doctor <remembersoftwares@gmail.com>
2025-01-31 16:06:00 +0100

    WIP tool calling untested

--- src/yacana/inference.py
+++ src/yacana/inference.py
@@ -43,34 +43,23 @@ class VllmInference(Inference):
 class OpenAIInference(Inference):
     def go(self, model_name: str, history: list, endpoint: str, api_token: str, model_settings: dict, stream: bool, json_output: bool, headers: dict, tools: List[Tool] | None = None):
         refusal: bool = False
-        function_calling_json = [tool._openai_function_schema for tool in tools] if tools else []
-        all_optional = all(tool.optional for tool in tools)
-        all_required = all(not tool.optional for tool in tools)
+        # Extracting all json schema from tools so it can be passed to the OpenAI API
+        all_function_calling_json = [tool._openai_function_schema for tool in tools] if tools else []
+        tool_choice = self._find_right_tool_choice_option(tools)
 
-        if all_optional:
-            tool_choice = ChatCompletionToolChoiceOptionParam.auto
-        elif all_required:
-            tool_choice = ChatCompletionToolChoiceOptionParam.required
-        else:
-            raise IllogicalConfiguration("OpenAI does not allow mixing required and optional tools.")
-        """
-        on tourne sur tous les tools.
-        Si ils sont tous optionnal = False donc le mode final est "required"
-        si ils sont tous à optional = True alors le mode final est "auto"
-        si y a un mixte de required et optional alors on raise IllogicalConfiguration() avec comme message que OpenAI ne permet pas de mixer des tools required et optional.
-        """
         response_format = {"type": "json_object"} if json_output else {}
 
         client = OpenAI(
             api_key=api_token,
         )
 
+        # @todo pour stream faudrait du code spécifique donc je ne vois pas bien comment on pourrait le faire
         completion = client.chat.completions.create(
             model=model_name,
             messages=history,
             stream=stream,
             response_format=response_format,
-            tools=function_calling_json,
+            tools=all_function_calling_json,
             tool_choice=tool_choice
         )
         refusal = False if completion.choices[0].message.refusal is None else True # Utile uniquement lorsqu'on utilise structured output. Permet de savoir si le modèle a refusé de répondre ou non pendant qu'on lui demande du JSON.
@@ -81,6 +70,22 @@ class OpenAIInference(Inference):
 
         return completion.choices[0].message.content
 
+    def _find_right_tool_choice_option(self, tools: List[Tool] | None) -> ChatCompletionToolChoiceOptionParam:
+        """
+        iterate over all tools, then:
+        If they are all optional == False then the final mode is "required"
+        If they are all optional == True then the final mode is "auto"
+        If there is a mix of required and optional, then raise IllogicalConfiguration() with a message explaining that mixing required and optional tools is not allowed for OpenAI.
+        """
+        all_optional = all(tool.optional for tool in tools)
+        all_required = all(not tool.optional for tool in tools)
+
+        if all_optional:
+            return ChatCompletionToolChoiceOptionParam.auto
+        elif all_required:
+            return ChatCompletionToolChoiceOptionParam.required
+        else:
+            raise IllogicalConfiguration("OpenAI does not allow mixing required and optional tools.")
 
 class InferenceFactory:
     @staticmethod
